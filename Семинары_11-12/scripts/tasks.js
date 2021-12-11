7f793c85131f6b8be863d675bd6c2420acbdf125
Andre <andrey.asdf2014@gmail.com>
2021-12-11 04:53:28 +0300

    all tasks

--- Семинары_11-12/scripts/tasks.js
+++ Семинары_11-12/scripts/tasks.js
@@ -164,3 +164,69 @@ RangeSeq.prototype.current = function() {
 };
 
 logFive(new RangeSeq(100, 1000));
+
+//task 15
+console.log('\ntask 15');
+let Card = {
+    from : "Chicago",
+    to : "Boston",
+    Show : function(from = this.from, to = this.to) {
+        this.from = from;
+        this.to = to;
+        return this.from + " to " + this.to;
+    }
+};
+let c1 = "Екатеринбург, Москва";
+const c1_arr = c1.split(", ");
+console.log(Card.Show(c1_arr[0], c1_arr[1]));
+
+//task 16
+console.log('\ntask 16');
+class Human{
+  constructor(name, age, height){
+    this.name = name;
+    this.age = age;
+    this.height = height;
+  }
+  getInfo(){
+    return `${this.name}, ${this.age}, ${this.height}`;
+  }
+  get firstName () {
+    return this.name;
+  }
+}
+let humans = [
+  new Human("Коля", 23, 180),
+  new Human("Даша", 19, 170),
+  new Human("Ваня", 18, 192),
+  new Human("Петя", 45, 178),
+  new Human("Вася", 34, 197),
+  new Human("Джони", 40, 168),
+  new Human("Катя", 37, 160),
+  new Human("Петя", 29, 200),
+  new Human("Соня", 21, 172),
+  new Human("Женя", 25, 175)
+];
+for (let i = 0; i < humans.length; i++) {
+  console.log(humans[i].getInfo());
+}
+for (let i = 0; i < humans.length; i++) {
+  console.log(humans[i].firstName);
+}
+
+//task 18
+console.log('\ntask 18');
+const dt1 = new Date(2045, 0, 1, 0, 0, 0, 0);
+console.log(dt1);
+
+//task 19
+console.log('\ntask 19');
+let dt2 = new Date();
+console.log(dt2);
+
+//task 20
+console.log('\ntask 20');
+function getDays(y, m){
+  return new Date(y, m, 0).getDate();
+}
+console.log(getDays(2021, 2));
